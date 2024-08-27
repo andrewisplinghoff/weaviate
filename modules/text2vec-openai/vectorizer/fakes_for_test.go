ed1f76ef7833094224d9104f1e37b1d296159734
Andre Wisplinghoff <andre.wisplinghoff@gmail.com>
2024-08-27 17:30:30 +0200

    Check that Vectorize() does not get called with an empty text array
    It does not make sense to do so and at least in the case of Azure OpenAI also leads to an error if we make a request without texts to be vectorized.

--- modules/text2vec-openai/vectorizer/fakes_for_test.go
+++ modules/text2vec-openai/vectorizer/fakes_for_test.go
@@ -36,7 +36,9 @@ func (c *fakeBatchClient) Vectorize(ctx context.Context,
 	if c.defaultResetRate == 0 {
 		c.defaultResetRate = 60
 	}
-
+	if len(text) == 0 {
+		panic("Vectorize() called with empty text array")
+	}
 	vectors := make([][]float32, len(text))
 	errors := make([]error, len(text))
 	rateLimit := &modulecomponents.RateLimits{RemainingTokens: 100, RemainingRequests: 100, LimitTokens: 200, ResetTokens: time.Now().Add(time.Duration(c.defaultResetRate) * time.Second), ResetRequests: time.Now().Add(time.Duration(c.defaultResetRate) * time.Second)}
@@ -59,6 +61,13 @@ func (c *fakeBatchClient) Vectorize(ctx context.Context,
 			header := make(http.Header)
 			header.Add("x-ratelimit-remaining-tokens", strconv.Itoa(rate))
 			rateLimit = ent.GetRateLimitsFromHeader(header)
+		}
+
+		if text[i] == "azure_429_too_many_requests" {
+			header := make(http.Header)
+			header.Add("x-ratelimit-reset-tokens", "10") // Meaning tokens will be reset in one second
+			rateLimit = ent.GetRateLimitsFromHeader(header)
+			errors[i] = fmt.Errorf("429 Too Many Requests")
 		}
 
 		req := len("requests ")
